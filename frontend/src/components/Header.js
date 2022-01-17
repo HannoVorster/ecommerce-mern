eef6321ef97d4a86a1516aa2c015e40e02e87df4
Hanno <vorster.hanno@protonmail.com>
2022-01-17 22:30:02 +0200

    Styling

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
     return (
         <header>
-            Header
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link href="/cart"><i class="fas fa-shopping-cart"></i>&nbsp;Cart</Nav.Link>
+                            <Nav.Link href="/login"><i class="fas fa-user"></i>&nbsp;Sign In</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </header>
     )
 }
